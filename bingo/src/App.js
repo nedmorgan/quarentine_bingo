babe50b4a312636d840cd2a9b67943a097efb7e7
nedmorgan <nmorgan1987@gmail.com>
2020-03-25 11:59:11 -0400

    added game rules

--- bingo/src/App.js
+++ bingo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Board from './components/Board'
+import Rules from './components/Rules'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Quarentine</h1>
+      <h1 className='game-title'>Quarentine</h1>
+      <Rules />
       <Board />
     </div>
   )
--- bingo/src/styles/RulesStyles.jsx
+++ bingo/src/styles/RulesStyles.jsx
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const RulesContainer = styled.div``
+export const RulesContainer = styled.div`
+  width: 250px;
+  height: auto;
+  position: absolute;
+  font-family: 'Roboto', sans-serif;
+  margin-left: 2em;
+
+  h2 {
+    margin: 0;
+    text-decoration: underline;
+  }
+
+  li {
+    text-align: left;
+    margin-top: 0.2em;
+  }
+`
